7340db8d2e00c94d2a4323104bfe54279f93a632
federicobal <federicoraulbal@gmail.com>
2022-01-10 15:57:57 -0300

    Home Map

--- app/src/main/java/com/project/appcleanarchitecture/ui/home/HomeFragment.kt
+++ app/src/main/java/com/project/appcleanarchitecture/ui/home/HomeFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.clustering.ClusterManager
+import com.project.appcleanarchitecture.Application
 import com.project.appcleanarchitecture.R
 import com.project.appcleanarchitecture.databinding.FragmentHomeBinding
+import com.project.appcleanarchitecture.ui.home.place.Place
+import com.project.appcleanarchitecture.ui.home.place.PlaceRenderer
+import com.project.appcleanarchitecture.ui.home.place.PlacesReader
 
 class HomeFragment : Fragment() {
 
@@ -19,7 +29,9 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private val places: List<Place> by lazy {
+        PlacesReader(Application.applicationContext()).read()
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,13 +43,120 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        initView()
+
+
         return root
     }
 
+    private fun initView()
+    {
+                val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        //val mapFragment = childFragmentManager.findFragmentById(binding.map.id) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            // Ensure all places are visible in the map
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                places.forEach { bounds.include(it.latLng) }
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+            }
+
+            //addMarkers(googleMap)
+            addClusteredMarkers(googleMap)
+
+            // Set custom info window adapter
+            // googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
+
+    }
+    /**
+     * Adds markers to the map with clustering support.
+     */
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer
+        val clusterManager = ClusterManager<Place>(Application.applicationContext(), googleMap)
+        clusterManager.renderer =
+            PlaceRenderer(
+                Application.applicationContext(),
+                googleMap,
+                clusterManager
+            )
+
+        // Set custom info window adapter
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(Application.applicationContext()))
+
+        // Add the places to the ClusterManager
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+
+        // Show polygon
+        clusterManager.setOnClusterItemClickListener { item ->
+            addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+
+        // When the camera starts moving, change the alpha value of the marker to translucent
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
+
+        googleMap.setOnCameraIdleListener {
+            // When the camera stops moving, change the alpha value back to opaque
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            // Call clusterManager.onCameraIdle() when the camera stops moving so that re-clustering
+            // can be performed when the camera stops moving
+            clusterManager.onCameraIdle()
+        }
+    }
+
+    private var circle: Circle? = null
+
+    /**
+     * Adds a [Circle] around the provided [item]
+     */
+    private fun addCircle(googleMap: GoogleMap, item: Place) {
+        circle?.remove()
+        circle = googleMap.addCircle(
+            CircleOptions()
+                .center(item.latLng)
+                .radius(1000.0)
+                .fillColor(ContextCompat.getColor(Application.applicationContext(), R.color.colorPrimaryTranslucent))
+                .strokeColor(ContextCompat.getColor(Application.applicationContext(), R.color.colorPrimary))
+        )
+    }
+
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(Application.applicationContext(), R.color.colorPrimary)
+        BitmapHelper.vectorToBitmap(Application.applicationContext(), R.drawable.ic_directions_bike_black_24dp, color)
+    }
+
+    /**
+     * Adds markers to the map. These markers won't be clustered.
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .icon(bicycleIcon)
+            )
+            // Set place as the tag on the marker object so it can be referenced within
+            // MarkerInfoWindowAdapter
+            marker!!.tag = place
+        }
+    }
+
+    companion object {
+        val TAG = HomeFragment::class.java.simpleName
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
